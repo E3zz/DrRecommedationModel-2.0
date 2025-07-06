3cba285fd10c68103521fe4156f880d87be3840d
Yahya <106545119+E3zz@users.noreply.github.com>
2025-07-07 02:56:01 +0500

    Update config.py

--- Flask/config.py
+++ Flask/config.py
@@ -1,2 +1,2 @@
 # Connection string is a global variable that is used in multiple files. 
-CONNECTION_STRING = "mongodb+srv://yahyakhalid1272:Cj%40123456@mydb.kudw48y.mongodb.net/"
+CONNECTION_STRING = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
